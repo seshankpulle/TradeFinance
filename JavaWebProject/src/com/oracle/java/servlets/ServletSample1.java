c241c06470d71f1433ab44e02ab6bff78bccf66f
seshankpulle <47845219+seshankpulle@users.noreply.github.com>
2019-03-28 16:16:01 +0530

    Update ServletSample1.java

--- JavaWebProject/src/com/oracle/java/servlets/ServletSample1.java
+++ JavaWebProject/src/com/oracle/java/servlets/ServletSample1.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 //This servlet is called from ServletSample1.jsp page through servlet mapping from web.xml 
 
 
+
 public class ServletSample1 extends HttpServlet {
 	
 	 private String message;
